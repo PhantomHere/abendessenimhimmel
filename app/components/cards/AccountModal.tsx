6ffcc73ede2e90d443af52445a4c3931b0a7093a
Phantom <151763272+PhantomHere@users.noreply.github.com>
2026-05-06 10:38:19 +0200

    bnm

--- app/components/cards/AccountModal.tsx
+++ app/components/cards/AccountModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { createClient } from "../lib/supabase/client";
+import { createClient } from "app/lib/supabase/client";
 import { useRouter } from "next/navigation";
 
 interface Props {
