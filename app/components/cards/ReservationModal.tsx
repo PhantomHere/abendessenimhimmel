2cb95c7cea1986a81d84fda4739aaabe24dbe1ad
Phantom <151763272+PhantomHere@users.noreply.github.com>
2026-06-03 09:35:53 +0200

    ыв

--- app/components/cards/ReservationModal.tsx
+++ app/components/cards/ReservationModal.tsx
@@ -220,6 +220,7 @@ export default function ReservationModal({ isOpen, onClose, cartItems, onSuccess
             guestName={guestName} selectedShip={selectedShip}
             selectedTable={selectedTable} flightDate={flightDate}
             onClose={handleClose}
+            prefersReduced={prefersReduced}
           />
         ) : (
           <>
